25266f61865d5dd117c63ce94061ff44d0e25e39
zikrullahcelep611 <zikrullahcelep@gmail.com>
2025-09-15 15:34:09 +0300

    Implement JwtService with token generation and parsing, including error handling for null and invalid tokens

---
+++ lab-report/backend/application/jwtService/jwtService.go
@@ -0,0 +1,82 @@
+package jwtservice
+
+import (
+	"net/http"
+	"time"
+
+	"gitbub.com/zikrullahcelep611/lab-report/backend/models/claims"
+	customErrors "gitbub.com/zikrullahcelep611/lab-report/backend/models/errors"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/rs/zerolog/log"
+)
+
+type JwtService struct {
+	jwtSecret []byte
+}
+
+func NewJwtService(jwtSecret string) *JwtService {
+	return &JwtService{jwtSecret: []byte(jwtSecret)}
+}
+
+func (j *JwtService) GetJwtKey() []byte {
+	return j.jwtSecret
+}
+
+func (j *JwtService) GenerateJwtToken(email string, expirationTime time.Time) (string, error) {
+	claims := &claims.Claims{
+		Email: email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(j.jwtSecret)
+	if err != nil {
+		log.Error().Str("operation", "GenerateJwtToken").Err(err).Msg("Could not create token")
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func (j *JwtService) ParseToken(tokenStr string) (*claims.Claims, error) {
+	if tokenStr == "" {
+		return nil, &customErrors.TokenIsNullError{Message: "Token is required"}
+	}
+
+	claims := &claims.Claims{}
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return j.GetJwtKey(), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, &customErrors.TokenIsInvalidError{Message: "Token is invalid"}
+	}
+
+	return claims, nil
+}
+
+func (j *JwtService) ParseTokenFromCookie(r *http.Request) (*claims.Claims, error) {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		return nil, err
+	}
+
+	return j.ParseToken(cookie.Value)
+}
+
+/*
+	Kullanıcı → Web Sitesi → Çerezde Token Saklanır
+                       ↓
+	Sonraki İsteklerde → ParseTokenFromCookie (çerezden token al)
+                       ↓  
+                   ParseToken (token'ı çöz ve kontrol et)
+                       ↓
+                   "Bu kullanıcı kimmiş? Ne yapabilir?"
+
+*/
--- lab-report/backend/models/errors/customErrors.go
+++ lab-report/backend/models/errors/customErrors.go
@@ -1,11 +1,25 @@
 package errors
 
-
-type  UserNotFoundError struct{
+type UserNotFoundError struct {
 	Message string
 }
 
-func (e *UserNotFoundError) Error() string{
+func (e *UserNotFoundError) Error() string {
 	return e.Message
 }
 
+type TokenIsNullError struct {
+	Message string
+}
+
+func (e *TokenIsNullError) Error() string {
+	return e.Message
+}
+
+type TokenIsInvalidError struct{
+	Message string
+}
+
+func (e *TokenIsInvalidError) Error() string{
+	return e.Message
+}
