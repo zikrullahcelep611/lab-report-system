9e700e5b6970b20b7c36fbfd19ce549b0aad1882
zikrullahcelep611 <zikrullahcelep@gmail.com>
2025-09-24 10:12:22 +0300

    feat: add role model and integrate role field in claims and user models

--- lab-report/backend/models/user/user.go
+++ lab-report/backend/models/user/user.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"gitbub.com/zikrullahcelep611/lab-report/backend/models/hospital"
+	"gitbub.com/zikrullahcelep611/lab-report/backend/models/role"
 	"gorm.io/gorm"
 )
 
@@ -9,11 +10,13 @@ import (
 
 type User struct {
 	gorm.Model
-	Firstname       string `json:"firstname"`
-	Lastname   string `json:"lastname"`
-	Email      string `json:"email"`
-	Password   string `json:"password"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
 
-	HospitalID uint `json:"hospital_id"`
-	Hospital hospital.Hospital `gorm:"foreignKey:hospitalID" json:"hospital"`
+	HospitalID uint              `json:"hospital_id"`
+	Hospital   hospital.Hospital `gorm:"foreignKey:HospitalID" json:"hospital"`
+
+	Role role.Role `json:"role" gorm:"type:varchar(20);default:'user'"`
 }
